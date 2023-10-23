fa3949666f00a00a651b3b904fe2ebf74f3b6b5f
Deasy Indrawati <indrawatideasy@gmail.com>
2023-10-23 11:25:16 +0700

    Delete folder directory

--- folder/read_product.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Mengambil data produk dari database atau sumber data lainnya
-// ...
-
-// Menampilkan data produk sebagai respons
-// ...
-
-// Biasanya, Anda akan menggunakan template atau antarmuka tampilan di sini
-?>
